05324d02f5e68208affa887d6fdafd14f27de123
Greg Fominykh <63728373+fominykhgreg@users.noreply.github.com>
2021-12-09 00:08:12 +0500

    Add files via upload

---
+++ menuScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.JoystickButton0))
+        {
+            SceneManager.LoadScene("Scene1");
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.JoystickButton1))
+        {
+            SceneManager.LoadScene("Scene2");
+
+        }
+        if (Input.GetKeyDown(KeyCode.JoystickButton2))
+        {
+            SceneManager.LoadScene("Scene3");
+
+        }
+    }
+}
