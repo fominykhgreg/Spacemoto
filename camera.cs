fd1dced2a32226ccdf1af8ee2a72fff6dfd1ef06
Greg Fominykh <63728373+fominykhgreg@users.noreply.github.com>
2021-12-09 00:03:36 +0500

    Add files via upload

---
+++ camera.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+
+    
+        GameObject player;
+
+        const float speedMove = 1.7f;
+
+        void Start()
+        {
+            player = GameObject.FindWithTag("Player");
+        }
+
+        void Update()
+        {
+            float direction = player.transform.position.x - transform.position.x;
+
+        if (direction < -0.2)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+
+        if (direction > 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        if (Mathf.Abs(direction) < 150)
+            {
+                Vector3 pos = transform.position;
+                pos.x += Mathf.Sign(direction) * speedMove * Time.deltaTime;
+                transform.position = pos;
+            }
+        }
+    }
+
---
+++ camerasm.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerasm : MonoBehaviour
+{
+    public GameObject player; // тут объект игрока
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+
+
+
+    //public Transform target;
+    //public float smooth = 5.0f;
+    //public Vector3 offset = new Vector3(0, 2, -5);
+    //void Update()
+    //{
+    //    transform.position = Vector3.Lerp(transform.position, target.position + offset, Time.deltaTime * smooth);
+    //}
+
+}
