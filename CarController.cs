de6dec91bb6eff81fdf28b445cfedaa116f9ca83
Greg Fominykh <63728373+fominykhgreg@users.noreply.github.com>
2021-12-09 00:04:23 +0500

    Add files via upload

---
+++ CarController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+    public int AddSpeed;
+    public int MaxSpeed;
+    public int MotorTorque = 10000;
+    public WheelJoint2D[] Wheels = new WheelJoint2D[2];
+    JointMotor2D Motor;
+    public int GetAxisRaw;
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+        
+
+        if (GetAxisRaw > 0 && Motor.motorSpeed > -MaxSpeed)
+        {
+            Motor.motorSpeed = (Motor.motorSpeed - AddSpeed) -Time.deltaTime;
+            Motor.maxMotorTorque = MotorTorque;
+            Wheels[0].motor = Motor;
+            Wheels[1].motor = Motor;
+        }
+        else if (GetAxisRaw < 0 && Motor.motorSpeed < MaxSpeed)
+        {
+            Motor.motorSpeed = (Motor.motorSpeed + AddSpeed) + Time.deltaTime;
+            Motor.maxMotorTorque = MotorTorque;
+            Wheels[0].motor = Motor;
+            Wheels[1].motor = Motor;
+        }
+        else if (GetAxisRaw == 0)
+        {
+            if (Motor.motorSpeed > 0)
+            {
+                Motor.motorSpeed = (Motor.motorSpeed - AddSpeed) - Time.deltaTime;
+                Wheels[0].motor = Motor;
+                Wheels[1].motor = Motor;
+            }
+            else if (Motor.motorSpeed < 0)
+            {
+                Motor.motorSpeed = (Motor.motorSpeed + AddSpeed) + Time.deltaTime;
+                Wheels[0].motor = Motor;
+                Wheels[1].motor = Motor;
+            }
+        }
+        
+
+    }
+    public void Move(int InputAxis)
+    {
+
+       GetAxisRaw= InputAxis;
+
+    }
+
+}
---
+++ endGame.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class endGame : MonoBehaviour
+{
+    public static int lifeValue = 1;
+    public static float deltaTime;
+    float score;
+    float timer1;
+    Text endGame1;
+    bool enableTimer = true;
+    float timer = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        endGame1 = GetComponent<Text>();
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        timer=0+ Time.deltaTime;
+
+        score += Time.deltaTime;
+
+        float res = ((int)(score * 10));
+       
+
+        if (lifeValue <= 0)
+        {
+            endGame1.text = res.ToString("#.##") + " ";
+            
+            
+            
+        }
+    }
+}
