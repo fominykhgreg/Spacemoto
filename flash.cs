4fc1666634f9ae46e76390d6022fbfbf5025d7dc
Greg Fominykh <63728373+fominykhgreg@users.noreply.github.com>
2021-12-09 00:05:38 +0500

    Add files via upload

---
+++ flash.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class flash : MonoBehaviour
+{
+
+    public float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= 0.5)
+        {
+            GetComponent<Text>().enabled = true;
+        }
+        if (timer >= 1)
+        {
+            GetComponent<Text>().enabled = false;
+            timer = 0;
+        }
+    }
+}
