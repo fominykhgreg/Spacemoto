79bd8ecf287e96272260e0524fd408a5c3e7024d
Greg Fominykh <63728373+fominykhgreg@users.noreply.github.com>
2021-12-08 23:57:55 +0500

    Add files via upload

---
+++ buttom.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttom : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.JoystickButton9))
+        {
+            SceneManager.LoadScene("Menu");
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            endGame.lifeValue = 1;
+        }
+    }
+}
