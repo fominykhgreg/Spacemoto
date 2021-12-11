67d0af364b86884b9cce168c75aeaa3904bddbc1
Greg Fominykh <63728373+fominykhgreg@users.noreply.github.com>
2021-12-11 20:23:33 +0500

    C# scripts

---
+++ powerUpE.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class powerUpE : MonoBehaviour
+{
+
+    public GameObject Player;
+
+    
+
+
+
+
+    void Start()
+    {
+        Player = GameObject.FindWithTag("koleso");
+    }
+
+
+    void OnCollisionEnter2D(Collision2D variable)
+    {
+        if (variable.gameObject == Player)
+        {
+            endGame.lifeValue -= 1;
+            Destroy(gameObject);
+            SceneManager.LoadScene("SceneE");
+        }
+
+    }
+}
+
