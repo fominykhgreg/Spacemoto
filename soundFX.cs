2c9b96a464e57b8f210bf6b972b235f6e6b63bdb
Greg Fominykh <63728373+fominykhgreg@users.noreply.github.com>
2021-12-12 12:30:34 +0500

    Sound script

---
+++ soundFX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class soundFX : MonoBehaviour
+{
+    public AudioSource AudioS;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioS = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            AudioS.Play();
+        }
+    }
+}
