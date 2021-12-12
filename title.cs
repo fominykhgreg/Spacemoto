ab37ccc1ca87f8e321303161385cec90bfbea679
Greg Fominykh <63728373+fominykhgreg@users.noreply.github.com>
2021-12-12 17:13:39 +0500

    SCript for title

---
+++ title.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class title : MonoBehaviour
+{
+    
+    public static int titleValue = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void zapusk()
+    {
+        Application.LoadLevel("Scene1");
+    }
+        // Update is called once per frame
+        void Update()
+    {
+        if (Input.anyKey)
+        {
+            //zapusk();
+            Invoke("zapusk", 1);
+            titleValue += 1;
+            
+        }
+    }
+}
