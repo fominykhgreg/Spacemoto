034923c3f864e8a096930e1bcf1222f1a7a5e784
Greg Fominykh <63728373+fominykhgreg@users.noreply.github.com>
2021-12-12 11:50:39 +0500

    Script  for restart

---
+++ Restart.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+
+    public GameObject Player;
+
+
+
+
+
+
+    void Start()
+    {
+        Player = GameObject.FindWithTag("koleso");
+    }
+
+
+    public void Restart1()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+        //SceneManager.GetActiveScene();
+        //SceneManager.LoadScene("Scene1");
+
+    }
+}
+
