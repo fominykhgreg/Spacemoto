01fd0862cfb41babcc75cc8dda6cf7005df79c67
Greg Fominykh <63728373+fominykhgreg@users.noreply.github.com>
2021-12-14 23:40:40 +0500

    Add files via upload

---
+++ zatemnenietitle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class zatemnenietitle : MonoBehaviour
+{
+
+
+    public Image AlphaImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //zkonectitle.zastavkaValue = 0;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (title.titleValue > 0)
+        {
+
+            AlphaImage.color = new Color(AlphaImage.color.r, AlphaImage.color.g, AlphaImage.color.b, AlphaImage.color.a + 1.2f * Time.deltaTime);
+        }
+    }
+}
